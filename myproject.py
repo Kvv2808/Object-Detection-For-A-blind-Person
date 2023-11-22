7225b56c2b7594eb049712388e896c4b4891b475
Kvv2808 <117533582+Kvv2808@users.noreply.github.com>
2023-11-22 17:13:39 +0000

    hjjhjkk

--- myproject.py
+++ myproject.py
@@ -1,30 +1,36 @@
 import cv2
+import pyttsx3
+text=pyttsx3.init()
+cap=cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
 
+classnames=[]
+classfile='coco.names'
+with open(classfile,'rt')as f:
+    classnames=f.read().strip('/n').spli('/n')
+configpath='ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightpath='frozen_inference_graph.pb'
 
-img= cv2.imread("hi.png")
-
-classNames= []
-classFile = "coco.names"
-with open(classFile,'rt') as f: 
-    classNames =f.read().rstrip('\n').split('\n')
-
-configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = 'frozen_inference_graph.pb'
-
-net = cv2.dnn_DetectionModel(weightsPath,configPath)
-
-net.setInputSize (320,320)
-net.setInputScale(1.0/ 127.5) 
-net. setInputMean ((127.5, 127.5, 127.5))
+net=cv2.dnn_DetectionModel(configpath,weightpath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/127.5)
+net.setInputMean((127.5,127.5,127.5))
 net.setInputSwapRB(True)
 
-
-classIds, confs, bbox = net.detect(img, confThreshold=0.5)
-print(classIds, bbox)
-
-for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox):
-    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-    cv2.putText(img, classNames[classId-1].upper(), (box [0]+10,box[1]+30),
-             cv2.FONT_HERSHEY_COMPLEX,1,(0,255.0),2) 
-    cv2.imshow("output",img)
-    cv2.waitKey(0)
+while True:
+    ret,img=cap.read()
+    classIds,confs,bbox=net.detect(img,confThreshold=0.5)
+    print(classIds,bbox)
+    if len(classIds)!=0:
+        for classId,confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            cv2.rectangle(img,box,color=(0,25,5,0),thickness=2)
+            cv2.putText(img,classnames[classId-1].upper(),(box[0]+10,box[1]+30))
+            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2
+    cv2.imshow('output',img)
+    cv2.waitKey(1)
+    ans=classnames[classId-1]
+    rate=40
+    text.setProperty('rate',rate)
+    text.say(ans)
+    text.runAndWait()
